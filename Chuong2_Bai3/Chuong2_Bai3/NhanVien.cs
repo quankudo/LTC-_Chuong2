93094240833a042d29775c72014747f50ce28990
quankudo <quannguyen2004aa@gmail.com>
2025-02-26 21:03:43 +0700

    Complete Bai3 Chuong2 enum and optional parameter

---
+++ Chuong2_Bai3/Chuong2_Bai3/NhanVien.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuong2_Bai3
+{
+    internal class NhanVien
+    {
+        public string Ten { get; set; }
+        public CapBacNhanVien CapBac { get; set; }
+        public double LuongCoBan { get; set; }
+
+        public NhanVien(string ten, CapBacNhanVien capBac, double luongCoBan)
+        {
+            Ten = ten;
+            CapBac = capBac;
+            LuongCoBan = luongCoBan;
+        }
+
+        public double TinhLuong(double heSoThuong = 1.0)
+        {
+            return LuongCoBan * heSoThuong;
+        }
+
+        public void HienThiThongTin()
+        {
+            Console.WriteLine($"Tên: {Ten}, Cấp bậc: {CapBac}, Lương cơ bản: {LuongCoBan} USD");
+        }
+    }
+}
--- Chuong2_Bai3/Chuong2_Bai3/Program.cs
+++ Chuong2_Bai3/Chuong2_Bai3/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace Chuong2_Bai3
 {
+    enum CapBacNhanVien
+    {
+        Intern = 1,
+        Junior = 2,
+        Mid = 3,
+        Senior = 4,
+        Lead = 5
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            NhanVien nv1 = new NhanVien("Nguyễn Hữu Quân", CapBacNhanVien.Mid, 2000);
+            NhanVien nv2 = new NhanVien("Lê Thị Diệu Nhi", CapBacNhanVien.Senior, 4000);
+
+            nv1.HienThiThongTin();
+            nv2.HienThiThongTin();
+
+            Console.WriteLine($"Lương của {nv1.Ten}: {nv1.TinhLuong()} USD");
+
+            Console.WriteLine($"Lương của {nv2.Ten} (hệ số thưởng 1.2): {nv2.TinhLuong(1.2)} USD");
+            Console.ReadKey();
         }
     }
 }
